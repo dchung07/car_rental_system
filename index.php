712f33e3ad3a0e2ebf15f33fdceb5575a8d460a7
David Chung <davidchung0704@gmail.com>
2024-04-07 21:26:07 +1000

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Car Rental System</title>
+</head>
+<body>
+    <!-- Header -->
+    <div class="container">
+        <div class="header">
+
+            <div class="logo">
+                <h3>Rent'a'Car</h3>
+                <img src="" alt="logo">
+            </div>
+
+
+        </div>
+    </div>
+
+    <!-- Content -->
+</body>
+</html>
