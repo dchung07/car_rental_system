7db473b2868505204b037fbedc914d1cd1101137
David Chung <davidchung0704@gmail.com>
2024-04-07 21:52:25 +1000

    setting up header

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Car Rental System</title>
+    <link rel="stylesheet" href="styles.css"/>
 </head>
 <body>
     <!-- Header -->
@@ -12,12 +13,29 @@
 
             <div class="logo">
                 <h3>Rent'a'Car</h3>
-                <img src="" alt="logo">
+                <img src="images/car.png" alt="logo">
             </div>
 
+            <div class="header_middle">
+
+            <form class="categoryForm" method="POST" onchange="submitForm()">
+                <select class="categoryDropdown" name="categoryDropdown" id="category">
+                    <option value="Category">Category</option>
+                    <option value="Fruit">Toyota</option>
+                    <option value="Drinks">Hyundai</option>
+                    <option value="Meat">Tesla</option>
+                </select>
+            </form>
+
+    
+            <form class="searchbar" action="index.php" method="POST">
+                    <input type="search" placeholder = "Search Products..." name="search"/>
+                    <button type="submit" name="searchSubmit" value="true" style="border: none; background: url('images/search_black_24dp.svg') no-repeat; width: 24px; height: 24px; cursor: pointer;"></button>
+                </form>
+            </div>
 
         </div>
-    </div>
+    <div class="content">
 
     <!-- Content -->
 </body>
