a09021d061d62b300241f0b552a3ea22351a6a97
David Chung <davidchung0704@gmail.com>
2024-04-24 19:47:05 +1000

    Styling changes to cards

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ document.getElementById('searchInput').addEventListener('input', searchFilter);
 function searchFilter() {
     const searchInput = document.getElementById('searchInput').value.toLowerCase().trim();
     const cardContainer = document.getElementById('cardContainer');
-    cardContainer.innerHTML = ''; 
+    cardContainer.innerHTML = '';
 
     const xhttp = new XMLHttpRequest();
 
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (xhttp.readyState === XMLHttpRequest.DONE) {
             if (xhttp.status === 200) {
                 const carsData = JSON.parse(xhttp.responseText);
@@ -15,21 +15,24 @@ function searchFilter() {
 
                     const searchInt = parseInt(searchInput);
 
-                    return car.availability === true &&
-                        (car.brand.toLowerCase().includes(searchInput) ||
-                         car.model.toLowerCase().includes(searchInput) ||
-                         car.type.toLowerCase().includes(searchInput) ||
-                         car.mileage.toLowerCase().includes(searchInput) ||
-                         car.fuel_type.toLowerCase().includes(searchInput) ||
-                         car.description.toLowerCase().includes(searchInput) ||
-                         (searchInt && car.seats === searchInt)
-                        );
+                    return car.brand.toLowerCase().includes(searchInput) ||
+                        car.model.toLowerCase().includes(searchInput) ||
+                        car.type.toLowerCase().includes(searchInput) ||
+                        car.mileage.toLowerCase().includes(searchInput) ||
+                        car.fuel_type.toLowerCase().includes(searchInput) ||
+                        car.description.toLowerCase().includes(searchInput) ||
+                        (searchInt && car.seats === searchInt);
                 });
 
-                if(filteredCars.length === 0) {
-                    
+                if (filteredCars.length === 0) {
+
+                    cardContainer.textContent = '';
+
                     carsData.forEach(car => {
-                        const cardHtml = `
+                        let cardHtml = '';
+
+                        if (car.availability === false) {
+                            cardHtml = `
                             <div class="card">
                                 <div class="car-image">
                                     <img src="car_images/${car.image}" alt="${car.brand} ${car.model}">
@@ -42,16 +45,40 @@ function searchFilter() {
                                     <p>Seats: ${car.seats}</p>
                                     <p>Price per Day: $${car.price_per_day}</p>
                                     <p>Description: ${car.description}</p>
-                                    <button onclick="addToCart('${car.brand}', '${car.model}')">Add to Cart</button>
+                                    <button class="unavailable_addToCartBtn" disabled>Rent</button>
                                 </div>
                             </div>
                         `;
+                        } else {
+                            cardHtml = `
+                            <div class="card">
+                                <div class="car-image">
+                                    <img src="car_images/${car.image}" alt="${car.brand} ${car.model}">
+                                </div>
+                                <div class="car-details">
+                                    <h3>${car.brand} ${car.model}</h3>
+                                    <p>Type: ${car.type}</p>
+                                    <p>Mileage: ${car.mileage}</p>
+                                    <p>Fuel Type: ${car.fuel_type}</p>
+                                    <p>Seats: ${car.seats}</p>
+                                    <p>Price per Day: $${car.price_per_day}</p>
+                                    <p>Description: ${car.description}</p>
+                                    <button class="addToCartBtn">Rent</button>
+                                </div>
+                            </div>
+                        `;
+                        }
                         cardContainer.insertAdjacentHTML('beforeend', cardHtml);
                     });
 
                 } else {
+                    cardContainer.textContent = '';
+
                     filteredCars.forEach(car => {
-                        const cardHtml = `
+                        let cardHtml = '';
+
+                        if (car.availability === false) {
+                            cardHtml = `
                             <div class="card">
                                 <div class="car-image">
                                     <img src="car_images/${car.image}" alt="${car.brand} ${car.model}">
@@ -64,10 +91,29 @@ function searchFilter() {
                                     <p>Seats: ${car.seats}</p>
                                     <p>Price per Day: $${car.price_per_day}</p>
                                     <p>Description: ${car.description}</p>
-                                    <button onclick="addToCart('${car.brand}', '${car.model}')">Add to Cart</button>
+                                    <button class="unavailable_addToCartBtn" disabled>Rent</button>
                                 </div>
                             </div>
                         `;
+                        } else {
+                            cardHtml = `
+                            <div class="card">
+                                <div class="car-image">
+                                    <img src="car_images/${car.image}" alt="${car.brand} ${car.model}">
+                                </div>
+                                <div class="car-details">
+                                    <h3>${car.brand} ${car.model}</h3>
+                                    <p>Type: ${car.type}</p>
+                                    <p>Mileage: ${car.mileage}</p>
+                                    <p>Fuel Type: ${car.fuel_type}</p>
+                                    <p>Seats: ${car.seats}</p>
+                                    <p>Price per Day: $${car.price_per_day}</p>
+                                    <p>Description: ${car.description}</p>
+                                    <button class="addToCartBtn">Rent</button>
+                                </div>
+                            </div>
+                        `;
+                        }
                         cardContainer.insertAdjacentHTML('beforeend', cardHtml);
                     });
                 }
@@ -76,7 +122,9 @@ function searchFilter() {
             }
         }
     };
-    
+
     xhttp.open('GET', 'cars.json', true);
     xhttp.send();
 }
+
+//
--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Car Rental System</title>
-    <link rel="stylesheet" href="styles.css"/>
+    <link rel="stylesheet" href="styles.css" />
     <script src="index.js" defer></script>
 </head>
+
 <body>
     <!-- Header -->
     <div class="container">
@@ -28,44 +30,79 @@
                     </select>
                 </form>
 
-        
-                <form class="searchbar" action="index.php" method="POST">
-                        <input type="search" placeholder = "Search Products..." name="search" id="searchInput"/>
-                        <button id = "submitSearch" type="button" name="searchSubmit" value="true" style="border: none; background: url('images/search_black_24dp.svg') no-repeat; width: 24px; height: 24px; cursor: pointer;"></button>
-                </form>
+
+                <div class="searchbar">
+                    <input type="search" placeholder="Search Products..." name="search" id="searchInput" />
+                    <button id="submitSearch" type="button" name="searchSubmit" value="true"
+                        style="border: none; background: url('images/search_black_24dp.svg') no-repeat; width: 24px; height: 24px; cursor: pointer;"></button>
+                </div>
+
+                <div id="suggestions"></div>
+
             </div>
 
-            <img src="images/cart.svg" alt="">
+            <img src="images/booking.png" alt="">
 
         </div>
-        
-    <div class="content">
 
-    <div class="card-container" id="cardContainer">
-        <?php
-            $carsJson = file_get_contents('cars.json');
-            $cars = json_decode($carsJson, true);
-            foreach ($cars as $car) {
-                if ($car['availability'] == 'true') {
-                    echo '<div class="card">';
+        <div class="content">
+
+            <div class="category_container">
+                <button>All Cars</button>
+                <select>Brands</select>
+                <select>Car Type</select>
+                <select></select>
+            </div>
+
+
+            <div class="card-container" id="cardContainer">
+                <?php
+                $carsJson = file_get_contents('cars.json');
+                $cars = json_decode($carsJson, true);
+                foreach ($cars as $car) {
+                    if ($car['availability'] == 'true') {
+                        echo '<div class="card">';
+
                         echo '<div class="car-image">';
-                            echo '<img src="car_images/' . $car['image'] . '" alt="' . $car['brand'] . ' ' . $car['model'] . '">';
+                        echo '<img src="car_images/' . $car['image'] . '" alt="' . $car['brand'] . "image" . ' ' . $car['model'] . '">';
+                        echo '</div>';
+
+                        echo '<div class="car-details">';
+                        echo '<h3>' . $car['brand'] . ' ' . $car['model'] . '</h3>';
+                        echo '<p>Type: ' . $car['type'] . '</p>';
+                        echo '<p>Mileage: ' . $car['mileage'] . '</p>';
+                        echo '<p>Fuel Type: ' . $car['fuel_type'] . '</p>';
+                        echo '<p>Seats: ' . $car['seats'] . '</p>';
+                        echo '<p>Price per Day: $' . $car['price_per_day'] . '</p>';
+                        echo '<p>Description: ' . $car['description'] . '</p>';
+                        echo '<button class="addToCartBtn">Rent</button>';
+
+                        echo '</div>';
+
+                        echo '</div>';
+                    } else {
+                        echo '<div class="unavailable_card">';
+                        echo '<div class="car-image">';
+                        echo '<img src="car_images/' . $car['image'] . '" alt="' . $car['brand'] . "image" . ' ' . $car['model'] . '">';
                         echo '</div>';
                         echo '<div class="car-details">';
-                            echo '<h3>' . $car['brand'] . ' ' . $car['model'] . '</h3>';
-                            echo '<p>Type: ' . $car['type'] . '</p>';
-                            echo '<p>Mileage: ' . $car['mileage'] . '</p>';
-                            echo '<p>Fuel Type: ' . $car['fuel_type'] . '</p>';
-                            echo '<p>Seats: ' . $car['seats'] . '</p>';
-                            echo '<p>Price per Day: $' . $car['price_per_day'] . '</p>';
-                            echo '<p>Description: ' . $car['description'] . '</p>';
-                            echo '<button onclick="addToCart(\'' . $car['brand'] . '\', \'' . $car['model'] . '\')">Add to Cart</button>';
+                        echo '<h3>' . $car['brand'] . ' ' . $car['model'] . '</h3>';
+                        echo '<p>Type: ' . $car['type'] . '</p>';
+                        echo '<p>Mileage: ' . $car['mileage'] . '</p>';
+                        echo '<p>Fuel Type: ' . $car['fuel_type'] . '</p>';
+                        echo '<p>Seats: ' . $car['seats'] . '</p>';
+                        echo '<p>Price per Day: $' . $car['price_per_day'] . '</p>';
+                        echo '<p>Description: ' . $car['description'] . '</p>';
+                        echo '<button class="unavailable_addToCartBtn" disabled>Not Available</button>';
                         echo '</div>';
-                    echo '</div>';
+                        echo '</div>';
+                    }
                 }
-            }
-        ?>
+                ?>
+            </div>
+        </div>
     </div>
 
 </body>
+
 </html>
